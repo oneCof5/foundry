e2f6c3f53732cba43a417c5926e0ae2b6553d8ef
Chris Pearson <oneCof5@gmail.com>
2021-11-05 13:47:16 -0400

    cleanup

--- Classes/Blood Hunter/rite-dawn-itemacro.js
+++ Classes/Blood Hunter/rite-dawn-itemacro.js
@@ -1,16 +1,14 @@
-/* 
+/*
 ItemMacro attached to the actual item the Rite of Dawn has been applied to via the rite-dawn.js and DAE effect
 */
-async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }\nif (args[0].hitTargets[0] === undefined) return;\nconst lastArg = args[args.length - 1];
-
+async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }
+const lastArg = args[args.length - 1];
 if (lastArg.hitTargets[0].length === 0) return {};
-
 let tokenD = canvas.tokens.get(lastArg.tokenId).actor;
 let actorD = game.actors.get(lastArg.actor._id);
-const target = canvas.tokens.get(lastArg.hitTargets[0].id);
-
+const targetD = canvas.tokens.get(lastArg.hitTargets[0].id);
 // Rite of Dawn deals additional damage to undead. Is the target 'undead'
-const undead = target.actor.data.type === "npc" ? ["undead"].some(value => (target.actor.data.data.details.type.value || "").toLowerCase().includes(value)) : ["undead"].some(race => (target.actor.data.data.details.race || "").toLowerCase().includes(race));
+const undead = targetD.actor.data.type === "npc" ? ["undead"].some(value => (targetD.actor.data.data.details.type.value || "").toLowerCase().includes(value)) : ["undead"].some(race => (targetD.actor.data.data.details.race || "").toLowerCase().includes(race));
 
 // determine hemocrit die that scales by Blood Hunter level
 const level = actorD.data.type === "npc" ? actorD.data.data.details.cr : actorD.classes["blood-hunter"].data.data.levels;
@@ -19,9 +17,8 @@ let baseDie = 1;
 let damage_type = "radiant";
 
 if(undead) { baseDie += 1;} // if we hit an undead, roll another
-let critDie = 2*baseDie; // default rules for crits
-let numDice = lastArg.isCritical ? `${critDie}d${hemoDie}`  : `${baseDie}d${hemoDie}`;
-let damageRoll = new Roll(`${numDice}d6`).roll();
-game.dice3d?.showForRoll(damageRoll); // show the animated dice
+let numDice = lastArg.isCritical ? 2 * baseDie : baseDie;
+let damageRoll = new Roll(`${numDice}d${hemoDie}`).evaluate({async:false});
+game.dice3d.showForRoll(damageRoll); // show the animated dice
 // pass to Midi-QOL
-new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damage_type, [target], damageRoll, {flavor: `(${CONFIG.DND5E.damageTypes[damage_type]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList});
+new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damage_type, [targetD], damageRoll, {flavor: `(${CONFIG.DND5E.damageTypes[damage_type]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList});
