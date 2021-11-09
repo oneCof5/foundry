210eb850a4a21984d0d7ce0096e56d722452a46e
Chris Pearson <oneCof5@gmail.com>
2021-11-09 12:33:22 -0500

    update to match rite-dawn.js

--- Classes/Blood Hunter/rite-storm.js
+++ Classes/Blood Hunter/rite-storm.js
@@ -1,9 +1,26 @@
+/*
+DAE Macro.execute
+
+Crimson Rite. As a bonus action, you can activate a crimson rite on a single
+  weapon with the elemental energy of a known rite of your choice that lasts
+  until you finish a short or long rest, or if you aren’t holding the weapon
+  at the end of your turn. When you activate a rite, you lose a number of hit
+  points equal to one roll of your hemocraft die, as shown in the Hemocraft Die
+  column of the Blood Hunter table.
+  For the duration, attacks from this weapon deal additional damage of the
+    chosen rite’s type. This damage is magical, and increases as you gain levels
+    as a blood hunter, as shown in the Hemocraft Die column of the Blood Hunter
+    table. A weapon can only hold a single active rite at a time.
+  Hemocraft Die by Level
+  1-4: 1d4 | 5-10: 1d6 | 11-16: 1d8 | 17+: 1d10
+*/
+
 const lastArg = args[args.length - 1];
 let actorD;
 if (lastArg.tokenId) actorD = canvas.tokens.get(lastArg.tokenId).actor;
 else actorD = game.actors.get(lastArg.actorId);
 let tokenD = canvas.tokens.get(lastArg.tokenId).actor;
-const target = canvas.tokens.get(lastArg.tokenId)
+const target = canvas.tokens.get(lastArg.tokenId);
 
 // Get Weapons
 let weapons = actorD.items.filter(i => i.data.type === `weapon`);
@@ -24,100 +41,49 @@ if (args[0] === "on") {
   // dialog option
   new Dialog({
     title: 'Rite of the Storm',
-    content: `
-    <style>
-    .rite-storm .form-group {
-        display: flex;
-        flex-wrap: wrap;
-        width: 100%;
-        align-items: flex-start;
-      }
-
-      .rite-storm .radio-label {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-        justify-items: center;
-        flex: 1 0 25%;
-        line-height: normal;
-      }
-
-      .rite-storm .radio-label input {
-        display: none;
-      }
-
-      .rite-storm img {
-        border: 0px;
-        width: 50px;
-        height: 50px;
-        flex: 0 0 50px;
-        cursor: pointer;
-      }
-
-      /* CHECKED STYLES */
-      .rite-storm [type=radio]:checked + img {
-        outline: 2px solid #f00;
-      }
-    </style>
-
-    <h3>Pick your weapon:</h3>
-
-    <form class="rite-storm">
-      <div class="form-group" id="weapons">
-       ${weapon_content}
-       </div>
-    </form>
-  `,
+    content: `<style>.rite-dawn .form-group {display: flex; flex-wrap: wrap; width: 100%; align-items: flex-start;} .rite-dawn .radio-label {display: flex; flex-direction: column; align-items: center; text-align: center; justify-items: center; flex: 1 0 25%; line-height: normal;} .rite-dawn .radio-label input {display: none;} .rite-dawn img {border: 0px; width: 50px; height: 50px; flex: 0 0 50px; cursor: pointer;} /* CHECKED STYLES */ .rite-dawn [type=radio]:checked + img {outline: 2px solid #f00;}</style><h3>Pick your weapon:</h3><form class="rite-dawn"><div class="form-group" id="weapons">${weapon_content}</div></form>`,
     buttons: {
       yes: {
         icon: '<i class="fas fa-check"></i>',
         label: 'Confirm',
         callback: async (html) => {
-          let weaponId = $("input[type='radio'][name='weapon']:checked").val();
-          let weapon = actorD.items.get(weaponId);
-          let copyWeapon = foundry.utils.duplicate(weapon);
-
-          let isMgc = copyWeapon.data.properties.mgc; // save current magic flag
-          copyWeapon.data.properties.mgc = true; // regardless, its now magic
-
-          copyWeapon.flags['midi-qol'].onUseMacroName = "ItemMacro"; // set the onUse to ItemMacro for this item
-
-          // update the ItemMacro (copied from a manually configured Rapier with a working ItemMacro)
-          copyWeapon.flags.itemacro = {
-            "macro": {
-                "data": {
-                    "_id": null,
-                    "name": weapon.name,
-                    "type": "script",
-                    "author": "5iqOgPLPmoUd9MiS",
-                    "img": weapon.img,
-                    "scope": "global",
-                    "command": "async function wait(ms) { return new Promise(resolve => { setTimeout(resolve, ms); }); }\nif (args[0].hitTargets[0] === undefined) return;\nconst lastArg = args[args.length - 1];\nif (lastArg.hitTargets[0].length === 0) return {};\nlet tokenD = canvas.tokens.get(lastArg.tokenId).actor;\nlet actorD = game.actors.get(lastArg.actor._id);\nconst target = canvas.tokens.get(lastArg.hitTargets[0].id);\nlet damage_type = \"lightning\";\nlet baseDie = 1;\nconst level = actorD.data.type === \"npc\" ? actorD.data.data.details.cr : actorD.classes[\"blood-hunter\"].data.data.levels;\nlet hemoDie = (4 + (2 * (Math.floor((level + 1) / 6))));\nlet critDie = 2*baseDie;\nlet attackDice = lastArg.isCritical ? `${critDie}d${hemoDie}`  : `${baseDie}d${hemoDie}`;\nlet damageRoll = new Roll(`${attackDice}`).evaluate({async:false});\ngame.dice3d?.showForRoll(damageRoll);\nnew MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damage_type, [target], damageRoll, {flavor: `(${CONFIG.DND5E.damageTypes[damage_type]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList});",
-                    "folder": null,
-                    "sort": 0,
-                    "permission": {
-                        "default": 0
-                    },
-                    "flags": {}
-                }
-            }
+/*
+// remove any preexisting Crimson Rite effects
+          const riteEffects = ["Crimson Rite of the Dawn","Crimson Rite of the Storm"];
+          let activeRites = target.actor.effects.filter(i => riteEffects.includes(i.data.label));
+          console.log("DEBUG activeRites", activeRites);
+          if ( activeRites.length > 0) {
+            console.log("Existing Crimson Rite Effect will be removed.");
+            const riteEffectIds = activeRites.map( e => e.id);
+            await target.actor.deleteEmbeddedDocuments('ActiveEffect', riteEffectIds);
           }
-          actorD.updateEmbeddedDocuments('Item', [copyWeapon])
+*/
+          // A weapon is chosen
+          const weaponId = $("input[type='radio'][name='weapon']:checked").val();
+          const weapon = await actorD.items.get(weaponId);
+          let copyWeapon = await foundry.utils.duplicate(weapon);
+          const weaponName = copyWeapon.name;
+          const isMgc = copyWeapon.data.properties.mgc;
+
+          // update values
+          copyWeapon.name = `${weaponName} (Rite of the Storm)`;
+          copyWeapon.data.properties.mgc = true;
+          copyWeapon.flags["midi-qol"] = { onUseMacroName: "rite-storm-weapon-attack" };
+
+          await actorD.updateEmbeddedDocuments('Item', [copyWeapon]);
           DAE.setFlag(actorD, 'flgRiteStorm', {
-            weaponID: weaponId,
+            wId: weaponId,
             isMagic: isMgc,
+            wName: weaponName
           });
 
-          // Hemocrit Die Value and Damage Type
+          // The Blood Hunter damages themselves
           const level = actorD.data.type === "npc" ? actorD.data.data.details.cr : actorD.classes["blood-hunter"].data.data.levels;
-          let hemoDie = (4 + (2 * (Math.floor((level + 1) / 6))));
-          let numDice = `1d${hemoDie}`
-          let damage_type = "lightning";
-          let damageRoll = new Roll(`${numDice}`).roll();
-
+          const hemoDie = (4 + (2 * (Math.floor((level + 1) / 6))));
+          const damage_type = "lightning";
+          let damageRoll = new Roll(`1d${hemoDie}`).evaluate({async:false});
           game.dice3d?.showForRoll(damageRoll);
-          new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damage_type, [target], damageRoll, {flavor: `(${CONFIG.DND5E.damageTypes[damage_type]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList});
+          await new MidiQOL.DamageOnlyWorkflow(actorD, tokenD, damageRoll.total, damage_type, [target], damageRoll, {flavor: `(${CONFIG.DND5E.damageTypes[damage_type]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList});
         }
       },
     },
@@ -125,16 +91,15 @@ if (args[0] === "on") {
 }
 
 if (args[0] === "off") {
-  let flag = await DAE.getFlag(actorD, 'flgRiteStorm')
-  console.log("FLAG ",flag);
-  let weapon = actorD.items.get(flag.weaponID);
-  let copyWeapon = foundry.utils.duplicate(weapon);
+  const flag = await DAE.getFlag(actorD, 'flgRiteStorm');
+  const weapon = actorD.items.get(flag.wId);
+  let copyWeapon = await foundry.utils.duplicate(weapon);
 
+  // update values
+  copyWeapon.name = `${flag.wName}`;
   copyWeapon.data.properties.mgc = flag.isMagic;
-  copyWeapon.flags['midi-qol'].onUseMacroName = "";
-  delete copyWeapon.flags.itemacro;
+  copyWeapon.flags["midi-qol"] = { onUseMacroName: null };
 
-  actorD.updateEmbeddedDocuments('Item', [copyWeapon])
-
+  await actorD.updateEmbeddedDocuments('Item', [copyWeapon]);
   DAE.unsetFlag(actorD, `flgRiteStorm`);
 }
